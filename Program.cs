ef1b88d739b66d0556a4f2cb6674100b5e7bbc47
klaudiaawronaa <43877735+klaudiaawronaa@users.noreply.github.com>
2020-02-29 01:27:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+/* ***** Author: Klaudia Wrona **** */
+/* ***** Converting file from .xml to .json **** */
+
+namespace swi
+{
+    class Program
+    {
+        const string PATH_INPUT = "input.xml";
+        const string PATH_OUTPUT = "output.json";
+        static XmlSerializer serializer;
+        static FileStream fileStream;
+        static Objects result;
+
+
+        static void Main(string[] args)
+
+        {
+
+            /* ***** DESERIALIZATION **** */
+            try
+            {
+                serializer = new XmlSerializer(typeof(Objects));
+                fileStream = new FileStream(PATH_INPUT, FileMode.Open);
+                result = (Objects)serializer.Deserialize(fileStream);
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+
+
+            /* ***** CONVERTING TO .JSON **** */
+            string JSONResult = JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
+            JSONResult = JSONResult.Replace("{", "");
+
+            foreach (string myString in JSONResult.Split(new string[] { Environment.NewLine }, 
+                    StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (myString.Contains("\"\": ["))
+                    JSONResult = JSONResult.Replace(myString, "");
+                if (myString.Contains("\":") && !myString.Contains(","))
+                    JSONResult = JSONResult.Replace(myString, myString + "{");
+                //will remove indents 
+                if (myString.Length >= 2)
+                    JSONResult = JSONResult.Replace(myString, myString.Substring(2));
+                   
+            }
+
+            JSONResult = JSONResult.Replace("{{", "[");
+            JSONResult = JSONResult.Replace("{", "");
+            JSONResult = JSONResult.Replace("[", "{");
+            JSONResult = JSONResult.Replace("]", "");
+            //^\s+$ will remove everything from the first blank line to the last (in a contiguous block of empty lines)
+            //[\r\n]* will remove the last LF -> End Of Line 
+            JSONResult = Regex.Replace(JSONResult, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
+            JSONResult = JSONResult.Insert(0, "{" + Environment.NewLine);
+            //will add first {
+
+
+            var tw = new StreamWriter(PATH_OUTPUT, false);
+                tw.WriteLine(JSONResult.ToString());
+                tw.Close();
+
+            Console.WriteLine("Conversion complete, see " + PATH_OUTPUT + " for resulting .JSON.");
+            Console.ReadKey();
+
+
+
+        }
+        
+
+       
+    }
+}
---
+++ config.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace swi
+{
+
+    [XmlRoot("objects")]
+    [JsonConverter(typeof(ObjectsConverter))]
+    public class Objects
+    {
+        [XmlElement("object")]
+        public List<Object> objects { get; set; }
+      
+   
+    }
+
+    [JsonConverter(typeof(ObjectConverter))]
+    public class Object
+    {
+        [XmlElement("obj_name")]
+        public string objectName { get; set; }
+
+        [XmlElement("field")]
+        public List<Field> fields { get; set; }
+
+    }
+
+    [JsonConverter(typeof(FieldConverter))]
+    public class Field
+    {
+        
+        [XmlElement("name")]
+        public string name { get; set; }
+        [JsonIgnore]
+        [XmlElement("type")]
+        public string type { get; set; }
+        [JsonIgnore]
+        [XmlElement(ElementName = "value")]
+        public string value { get; set; }
+
+    }
+
+
+    public class FieldConverter : JsonConverter
+    {
+        
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Field);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return new Field { name = reader.Value.ToString() };
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (((Field)value).value != "" && ((Field)value).name != "" &&
+               ((Field)value).value != null && ((Field)value).name != null)
+            {
+                if (((Field)value).type == "string")
+                    writer.WriteRawValue(" " + "\"" +((Field)value).name + "\": \"" + ((Field)value).value + "\"");
+                if (((Field)value).type == "int")
+                    writer.WriteRawValue(" " + "\""  + ((Field)value).name + "\": " + ((Field)value).value);
+
+            }
+        }
+    }
+
+    public class ObjectConverter : JsonConverter
+    {
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Object);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return new Object { objectName = reader.Value.ToString() };
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            IEnumerable array = ((Object)value).fields;
+            writer.WriteStartObject();
+            writer.WritePropertyName(((Object)value).objectName);
+            writer.WriteStartArray();
+            foreach (object item in array)
+            {
+                serializer.Serialize(writer, item);
+            }
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+    }
+
+    public class ObjectsConverter : JsonConverter
+    {
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Objects);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            IEnumerable array = ((Objects)value).objects;
+            writer.WriteStartObject();
+            writer.WritePropertyName("");
+            writer.WriteStartArray();
+            foreach (Object item in array)
+            {
+                if(item.objectName != " " && item.fields.Count()>=1) serializer.Serialize(writer, item);
+            }
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+    }
+}
